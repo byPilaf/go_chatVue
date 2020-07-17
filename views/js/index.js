f8608fd951d675aa317a4800c5c92d7426ce49f1
byPilaf <a492942809@live.com>
2020-07-17 17:28:28 +0800

    p

--- views/js/index.js
+++ views/js/index.js
@@ -65,19 +65,23 @@ var app = new Vue({
                     }
                 }, function (err) { })
         },
+
         ws: function () {
             var that = this
             var host = window.location.hostname + ":" + window.location.port
-            console.log(host)
-            var ws = new WebSocket("ws://" + host + "/ws?user_token=" + that.user_token)
+            var ws = new WebSocket("ws://" + host + "/wws?user_token=" + that.user_token)
             ws.onerror = function (err) {
-                //连接失败,重新登陆
-                that.$alert('请重新登陆', '提示', {
-                    confirmButtonText: '确定',
-                    callback: function () {
-                        window.location.replace("/login")
-                    }
-                });
+                that.$message.error("连接失败")
+                console.log("err")
+                //todo 等待函数 sleep
+                that.ws()
+                // //连接失败,重新登陆
+                // that.$alert('请重新登陆', '提示', {
+                //     confirmButtonText: '确定',
+                //     callback: function () {
+                //         window.location.replace("/login")
+                //     }
+                // });
             }
 
             ws.onmessage = function (event) {
